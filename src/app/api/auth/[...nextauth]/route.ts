10eb30305bbe342a8f7aaa72763ec5101233897d
Axel Samuelson <axel@samuelson.se>
2025-10-21 12:04:45 +0200

    Remove unused @ts-expect-error directive from NextAuth route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,3 @@
-// @ts-expect-error - NextAuth v5 beta compatibility issue with Next.js 15
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { auth } from '@/lib/auth-config'
 
 export const { GET, POST } = auth
